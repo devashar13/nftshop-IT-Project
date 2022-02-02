1153901885266d6b157270cfe1c7c5ed08477287
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2022-02-02 16:39:48 +0530

    feat:nft contract 🤑

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,8 +7,28 @@ import "@openzeppelin/contracts/token/utils/Counters.sol";
 contract NFT is ERC721URIStorage{
     // Auto incrementation of tokenid
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIds;
 
+    // Address of the NFT marketplace contract
+    address contractAddress;
+
+    constructor(address marketPlaceAddress) ERC721("Biryani Tokens","BIR){
+        contractAddress = marketPlaceAddress;
+    }
+
+    /// @dev Create a new NFT token
+    /// @param tokenURI: the token uri
+    function createToken(string memory) public returns(uint256)  {
+        // set a mew token id for the token to be minted
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+        
+    }
 
 }
 
